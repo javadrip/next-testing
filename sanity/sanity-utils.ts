729f4e24c4cb2314954da076b6ce560506c9f91e
Vinleon Ang <javadripdev@gmail.com>
2023-06-12 22:05:18 +0800

    Add getCategory and getCategoryPosts

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -2,7 +2,9 @@ import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
 import { Post } from "../types/Post";
 import { Author } from "../types/Author";
+import { Category } from "../types/Category";
 
+// Get a single post
 export async function getPost(category: string, post: string): Promise<Post> {
   // clientConfig is imported from sanity/config/client-config.ts
   return createClient(clientConfig).fetch(
@@ -32,6 +34,38 @@ export async function getPost(category: string, post: string): Promise<Post> {
   );
 }
 
+// Get a single category
+export async function getCategory(category: string): Promise<Category> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "category" && categorySlug.current == $category][0]{
+      _id,
+      _createdAt,
+      category,
+      "categorySlug": categorySlug.current,
+      description,
+    }`,
+    { category }
+  );
+}
+
+// Get all posts in a single category
+export async function getCategoryPosts(category: string): Promise<Post[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "post" && $category in categories[]->categorySlug.current]{
+      _id,
+      _createdAt,
+      title,
+      "postSlug": postSlug.current,
+      "categorySlug": categories[0]->categorySlug.current,
+      "mainImage": mainImage.asset->url,
+      "category": categories[0]->category,
+      publishedAt,
+    }`,
+    { category }
+  );
+}
+
+// Get data of a single author
 export async function getAuthor(authorSlug: string): Promise<Author> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "author" && authorSlug.current == $authorSlug][0]{
@@ -46,6 +80,7 @@ export async function getAuthor(authorSlug: string): Promise<Author> {
   );
 }
 
+// Get all posts by a single author
 export async function getAuthorPosts(authorSlug: string): Promise<Post[]> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "post" && references(*[_type == "author" && authorSlug.current == $authorSlug][0]._id)]{
@@ -55,7 +90,7 @@ export async function getAuthorPosts(authorSlug: string): Promise<Post[]> {
       "postSlug": postSlug.current,
       "categorySlug": categories[0]->categorySlug.current,
       "mainImage": mainImage.asset->url,
-      "category": *[_type == "category" && categorySlug.current == categories[0]->categorySlug.current][0].category,
+      "category": categories[0]->category,
       publishedAt,
     }`,
     { authorSlug }
