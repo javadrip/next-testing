d9fd9d54967d725ba20e72cd2955f0850e68df8e
Vinleon Ang <javadripdev@gmail.com>
2023-06-12 21:28:22 +0800

    Create Category type

---
+++ types/Category.ts
@@ -0,0 +1,10 @@
+export type Category = {
+  // All properties with an underscore are generated automatically by Sanity
+  _id: string;
+  _createdAt: Date;
+
+  // The rest of the properties are defined in the schema by us
+  category: string;
+  categorySlug: string;
+  description: string;
+};
