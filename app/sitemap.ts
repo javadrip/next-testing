7dc11e770c261b88f19d34ad2ea0aeeb3539e2fd
Vinleon Ang <javadripdev@gmail.com>
2023-06-14 15:12:41 +0800

    Create dynamic sitemap generation

---
+++ app/sitemap.ts
@@ -0,0 +1,51 @@
+import {
+  getAllPostSlugs,
+  getAllCategories,
+  getAllAuthorSlugs,
+} from "@/sanity/sanity-utils";
+import { MetadataRoute } from "next";
+
+// Remember to keep robots.txt up to date
+const URL = "https://next-testing-fawn.vercel.app";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const postsData = await getAllPostSlugs();
+  const categoriesData = await getAllCategories();
+  const authorsData = await getAllAuthorSlugs();
+
+  const posts = postsData.map(route => ({
+    url: `${URL}/${route.categorySlug}/${route.postSlug}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const categories = categoriesData.map(route => ({
+    url: `${URL}/${route.categorySlug}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const authors = authorsData.map(route => ({
+    url: `${URL}/authors/${route.authorSlug}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...posts, ...categories, ...authors];
+
+  // return [
+  //   {
+  //     url: URL,
+  //     lastModified: new Date(),
+  //   },
+  //   {
+  //     url: URL + "/fiction",
+  //     lastModified: new Date(),
+  //   },
+  //   {
+  //     url: URL + "/cat",
+  //     lastModified: new Date(),
+  //   },
+  //   {
+  //     url: URL + "/dog",
+  //     lastModified: new Date(),
+  //   },
+  // ];
+}
