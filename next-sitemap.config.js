50bf7b60211c46cc1682d421bc9d4392039eefc5
Vinleon Ang <javadripdev@gmail.com>
2023-06-14 13:13:01 +0800

    Add dynamic sitemap generation + robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  // siteUrl: process.env.SITE_URL || "https://example.com",
+  siteUrl: "next-testing-bviwslodr-javadrip.vercel.app",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
