eac75efc05b26543dfd34b9d5f2c0974f19faa4b
Vinleon Ang <javadripdev@gmail.com>
2023-07-10 18:08:33 +0800

    Edit Author types

--- types/Author.ts
+++ types/Author.ts
@@ -4,13 +4,14 @@ import { PortableTextBlock } from "sanity";
 export type Author = {
   // All properties with an underscore are generated automatically by Sanity
   _id: string;
-  _createdAt: Date;
+  _createdAt: string;
 
   // The rest of the properties are defined in the schema by us
   name: string;
-  authorSlug: string;
+  slug: {
+    current: string;
+  };
   image: string;
-  alt: string;
   bio: PortableTextBlock[];
   posts: Post[];
 };
