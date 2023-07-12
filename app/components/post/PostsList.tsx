0abc72acfa0686005340a9dc7eb806d80f4dc97d
Vinleon Ang <javadripdev@gmail.com>
2023-07-12 10:13:22 +0800

    Refactor posts components to 1 PostsList

--- app/components/post/PostsList.tsx
+++ app/components/post/PostsList.tsx
@@ -4,13 +4,12 @@ type Props = {
   promise: Promise<Post[]>;
 };
 
-export default async function CategoryPosts({ promise }: Props) {
+export default async function PostsList({ promise }: Props) {
   const posts = await promise;
 
   const content = posts.map(post => (
     <article key={post._id}>
       <h2>Post title: {post.title}</h2>
-      <br />
       Post slug: {post.slug.current} <br />
       Category: {post.categories[0].title} <br />
       Category slug: {post.categorySlug} <br />
--- app/components/post/TBDCategoryPosts.tsx
+++ app/components/post/TBDCategoryPosts.tsx
@@ -4,15 +4,14 @@ type Props = {
   promise: Promise<Post[]>;
 };
 
-export default async function AuthorPosts({ promise }: Props) {
+export default async function CategoryPosts({ promise }: Props) {
   const posts = await promise;
 
   const content = posts.map(post => (
     <article key={post._id}>
       <h2>Post title: {post.title}</h2>
-      <br />
-      Post slug: {post.postSlug} <br />
-      Category: {post.category} <br />
+      Post slug: {post.slug.current} <br />
+      Category: {post.categories[0].title} <br />
       Category slug: {post.categorySlug} <br />
       Date posted: {post._createdAt.toString()} <br />
       <br />
