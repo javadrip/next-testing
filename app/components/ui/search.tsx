4a01d09e896badf2ebed24261501935a78242902
Vinleon Ang <javadripdev@gmail.com>
2023-07-12 15:50:35 +0800

    Create search component

---
+++ app/components/ui/search.tsx
@@ -0,0 +1,27 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+interface Props {
+  //TODO: Check if Props is correct
+  q?: string;
+  handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+}
+
+export default function Search({ q, handleChange, placeholder }: Props) {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        defaultValue={q}
+        onChange={handleChange}
+        placeholder={placeholder}
+        name="q"
+        id="q"
+        className="w-full rounded-md border px-3 py-2 outline-none focus:border-gray-300 focus:shadow-sm dark:border-gray-600 dark:bg-gray-900 dark:focus:border-white"
+      />
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+        <MagnifyingGlassIcon className="h-4 w-4 text-gray-400" />
+      </div>
+    </div>
+  );
+}
