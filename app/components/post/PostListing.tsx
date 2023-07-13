782111cd2aa5bb3ef8f98c16a0d3fe736b75fbd6
Vinleon Ang <javadripdev@gmail.com>
2023-07-13 22:32:47 +0800

    Add style to PostListing

--- app/components/post/PostListing.tsx
+++ app/components/post/PostListing.tsx
@@ -1,18 +1,155 @@
+import Link from "next/link";
+import Image from "next/image";
+import { parseISO, format } from "date-fns";
+
+import clsx from "clsx";
+
 import type { Post } from "@/types/Post";
+import { PhotoIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   post: Post;
+  aspect?: "landscape" | "square" | "custom";
+  minimal?: boolean;
+  pathPrefix?: string;
+  preloadImage?: boolean;
+  fontSize?: "large" | "normal";
+  fontWeight?: "normal" | "bold";
 };
 
-export default function PostListing({ post }: Props) {
+export default function PostListing({
+  post,
+  aspect,
+  minimal,
+  pathPrefix,
+  preloadImage,
+  fontSize,
+  fontWeight,
+}: Props) {
   return (
-    <>
-      <h2>Post title: {post.title}</h2>
-      Post slug: {post.slug.current} <br />
-      Category: {post.categories[0].title} <br />
-      Category slug: {post.categorySlug} <br />
-      Date posted: {post._createdAt.toString()} <br />
-      <br />
-    </>
+    <div
+      className={clsx(
+        "group cursor-pointer",
+        minimal && "grid gap-10 md:grid-cols-2"
+      )}
+    >
+      <div
+        className={clsx(
+          " overflow-hidden rounded-md bg-gray-100 transition-all hover:scale-105   dark:bg-gray-800"
+        )}
+      >
+        <Link
+          className={clsx(
+            "relative block",
+            aspect === "landscape"
+              ? "aspect-video"
+              : aspect === "custom"
+              ? "aspect-[5/4]"
+              : "aspect-square"
+          )}
+          href={`/${post.categorySlug}/${pathPrefix ? `${pathPrefix}/` : ""}${
+            post.slug.current
+          }`}
+        >
+          {/* {imageProps ? (
+              <Image
+                src={imageProps.src}
+                alt={post.mainImage.alt || "Thumbnail"}
+                priority={preloadImage ? true : false}
+                className="object-cover transition-all"
+                fill
+                sizes="(max-width: 768px) 30vw, 33vw"
+              />
+            ) : (
+              <span className="absolute left-1/2 top-1/2 h-16 w-16 -translate-x-1/2 -translate-y-1/2 text-gray-200">
+                <PhotoIcon />
+              </span>
+            )} */}
+        </Link>
+      </div>
+
+      <div className={clsx(minimal && "flex items-center")}>
+        <div>
+          {/* <CategoryLabel categories={post.categories} nomargin={minimal} /> */}
+          <h2
+            className={clsx(
+              fontSize === "large"
+                ? "text-2xl"
+                : minimal
+                ? "text-3xl"
+                : "text-lg",
+              fontWeight === "normal"
+                ? "line-clamp-2 font-medium  tracking-normal text-black"
+                : "font-semibold leading-snug tracking-tight",
+              "mt-2    dark:text-white"
+            )}
+          >
+            <Link
+              href={`/${post.categorySlug}/${
+                pathPrefix ? `${pathPrefix}/` : ""
+              }${post.slug.current}`}
+            >
+              <span
+                className="bg-gradient-to-r from-green-200 to-green-100 bg-[length:0px_10px] bg-left-bottom
+      bg-no-repeat
+      transition-[background-size]
+      duration-500
+      hover:bg-[length:100%_3px]
+      group-hover:bg-[length:100%_10px]
+      dark:from-purple-800 dark:to-purple-900"
+              >
+                {post.title}
+              </span>
+            </Link>
+          </h2>
+
+          <div className="hidden">
+            {post.excerpt && (
+              <p className="mt-2 line-clamp-3 text-sm text-gray-500 dark:text-gray-400">
+                <Link
+                  href={`/${post.categorySlug}/${
+                    pathPrefix ? `${pathPrefix}/` : ""
+                  }${post.slug.current}`}
+                  legacyBehavior
+                >
+                  {post.excerpt}
+                </Link>
+              </p>
+            )}
+          </div>
+
+          <div className="mt-3 flex items-center space-x-3 text-gray-500 dark:text-gray-400">
+            <Link href={`/author/${post.author.slug.current}`} legacyBehavior>
+              <div className="flex items-center gap-3">
+                <div className="relative h-5 w-5 flex-shrink-0">
+                  {/* {AuthorimageProps && (
+                      <Image
+                        src={AuthorimageProps.src}
+                        alt={post?.author?.name}
+                        className="rounded-full object-cover"
+                        fill
+                        sizes="20px"
+                      />
+                    )} */}
+                </div>
+                <span className="truncate text-sm">{post.author.name}</span>
+              </div>
+            </Link>
+            <span className="text-xs text-gray-300 dark:text-gray-600">
+              &bull;
+            </span>
+            <time
+              className="truncate text-sm"
+              dateTime={post?.publishedAt || post._createdAt}
+            >
+              {format(
+                parseISO(post?.publishedAt || post._createdAt),
+                "MMMM dd, yyyy"
+              )}
+            </time>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
