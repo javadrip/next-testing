bf176239bf91b814fcbb0accef84a3950710ac9b
Vinleon Ang <javadripdev@gmail.com>
2023-07-18 20:44:49 +0800

    Create CategoryLabel

---
+++ app/components/ui/CategoryLabel.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import CategoryLabelStyles from "./CategoryLabelStyles";
+
+import { Category } from "@/types/Category";
+
+interface Props {
+  categories: Category[];
+  nomargin?: boolean;
+}
+
+export default function CategoryLabel({ categories, nomargin = false }: Props) {
+  return (
+    <div className="flex gap-3">
+      {categories?.length &&
+        categories.slice(0).map((category, index) => (
+          <Link href={`/category/${category.slug.current}`} key={index}>
+            <CategoryLabelStyles nomargin={nomargin} color={category.color}>
+              {category.title}
+            </CategoryLabelStyles>
+          </Link>
+        ))}
+    </div>
+  );
+}
