8ee4254f98729a94645567182e6f53ba0f7753b6
Vinleon Ang <javadripdev@gmail.com>
2023-07-27 17:06:02 +0800

    Add new navbar code

--- app/components/navigation/navbar.tsx
+++ app/components/navigation/navbar.tsx
@@ -1,155 +1,207 @@
 "use client";
 
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Disclosure } from "@headlessui/react";
 
-import { menu } from "./menu";
-import { DropdownMenu } from "./dropdownmenu";
-import Container from "../container";
+import Logo from "../../assets/Logo.png";
+import NavbarMenu from "./NavbarMenu";
 
-import Search from "../ui/search";
-import { Fragment } from "react";
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-export interface Props {
-  logo: {
-    asset: {
-      _ref: string;
-    };
-  };
-  logoalt: {
-    asset: {
-      _ref: string;
-    };
-  };
-  mobile: boolean;
-}
-
-export default function NavbarAlt(props: Props) {
   return (
-    <Container className="!py-0">
-      <nav className="my-4">
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <div className="flex flex-wrap justify-between md:gap-10 lg:flex-nowrap">
-                <div className="flex w-full items-center justify-between lg:w-auto">
-                  <Link href="/" className="w-28 dark:hidden">
-                    {/* {props.logo ? (
-                      <Image
-                        src={urlForImage(props.logo)?.src || ""}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">Stablo</span>
-                    )} */}
-                  </Link>
-                  <Link href="/" className="hidden w-28 dark:block">
-                    {/* {props.logoalt ? (
-                      <Image
-                        src={urlForImage(props.logoalt)?.src || ""}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">Stablo</span>
-                    )} */}
-                  </Link>
-                  <Disclosure.Button
-                    aria-label="Toggle Menu"
-                    className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 lg:hidden "
-                  >
-                    <svg
-                      className="h-6 w-6 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      {open && (
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                        />
-                      )}
-                      {!open && (
-                        <path
-                          fillRule="evenodd"
-                          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                        />
-                      )}
-                    </svg>
-                  </Disclosure.Button>
-                </div>
-                <div className="flex items-center gap-3">
-                  <div className="hidden w-full flex-col items-center lg:flex lg:w-auto lg:flex-row ">
-                    {menu.map((item, index) => (
-                      <Fragment key={index + item.label}>
-                        {item.children && item.children.length > 0 ? (
-                          <DropdownMenu
-                            menu={item}
-                            key={index + item.label}
-                            items={item.children}
-                            mobile={props.mobile}
-                          />
-                        ) : (
-                          <Link
-                            href={item.href}
-                            key={index + item.label}
-                            className="rounded-full px-5 py-2 font-medium text-gray-600 outline-none ring-blue-100 hover:text-blue-500 focus-visible:text-blue-500 focus-visible:ring-2 dark:text-gray-400"
-                            target={item.external ? "_blank" : ""}
-                            rel={item.external ? "noopener" : ""}
-                          >
-                            {item.label}
-                          </Link>
-                        )}
-                      </Fragment>
-                    ))}
-                  </div>
-                  <div className="hidden lg:block">
-                    <form action="/search" method="GET">
-                      <Search placeholder="Search" />
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <Disclosure.Panel>
-                <div className="order-2 -ml-5 mt-5 flex w-full flex-col items-start justify-start lg:hidden">
-                  {menu.map((item, index) => (
-                    <Fragment key={index + item.label}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={index + item.label}
-                          items={item.children}
-                          mobile={true}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={index + item.label}
-                          className="rounded-full px-5 py-2 text-sm font-medium text-gray-600 outline-none ring-blue-100 hover:text-blue-500 focus-visible:text-blue-500 focus-visible:ring-2 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}
-                        >
-                          {item.label}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                  <div className="mt-2 px-5">
-                    <form action="/search" method="GET">
-                      <Search placeholder="Search" />
-                    </form>
-                  </div>
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </nav>
-    </Container>
+    <nav className="bg-white">
+      <div className="flex items-center font-medium justify-around">
+        <div className="z-50 p-5 md:w-auto w-full flex justify-between">
+          <Image src={Logo} alt="logo" className="md:cursor-pointer h-9" />
+          <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
+            {/* <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon> */}
+          </div>
+        </div>
+        <ul className="md:flex hidden uppercase items-center gap-8 font-[Poppins]">
+          <li>
+            <Link href="/" className="py-7 px-3 inline-block">
+              Home
+            </Link>
+          </li>
+          <NavbarMenu />
+        </ul>
+        {/* Mobile nav */}
+        <ul
+          className={`
+        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        duration-500 ${open ? "left-0" : "left-[-100%]"}
+        `}
+        >
+          <li>
+            <Link href="/" className="py-7 px-3 inline-block">
+              Home
+            </Link>
+          </li>
+          <NavbarMenu />
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
+
+// ============================== ORIGINAL NAVBAR ============================== //
+
+// "use client";
+
+// import Link from "next/link";
+// import Image from "next/image";
+// import { Disclosure } from "@headlessui/react";
+
+// import { menu } from "./menu";
+// import { DropdownMenu } from "./dropdownmenu";
+// import Container from "../container";
+
+// import Search from "../ui/search";
+// import { Fragment } from "react";
+
+// export interface Props {
+//   logo: {
+//     asset: {
+//       _ref: string;
+//     };
+//   };
+//   logoalt: {
+//     asset: {
+//       _ref: string;
+//     };
+//   };
+//   mobile: boolean;
+// }
+
+// export default function NavbarAlt(props: Props) {
+//   return (
+//     <Container className="!py-0">
+//       <nav className="my-4">
+//         <Disclosure>
+//           {({ open }) => (
+//             <>
+//               <div className="flex flex-wrap justify-between md:gap-10 lg:flex-nowrap">
+//                 <div className="flex w-full items-center justify-between lg:w-auto">
+//                   <Link href="/" className="w-28 dark:hidden">
+//                     {/* {props.logo ? (
+//                       <Image
+//                         src={urlForImage(props.logo)?.src || ""}
+//                         alt="Logo"
+//                         priority={true}
+//                         sizes="(max-width: 640px) 100vw, 200px"
+//                       />
+//                     ) : (
+//                       <span className="block text-center">Stablo</span>
+//                     )} */}
+//                   </Link>
+//                   <Link href="/" className="hidden w-28 dark:block">
+//                     {/* {props.logoalt ? (
+//                       <Image
+//                         src={urlForImage(props.logoalt)?.src || ""}
+//                         alt="Logo"
+//                         priority={true}
+//                         sizes="(max-width: 640px) 100vw, 200px"
+//                       />
+//                     ) : (
+//                       <span className="block text-center">Stablo</span>
+//                     )} */}
+//                   </Link>
+//                   <Disclosure.Button
+//                     aria-label="Toggle Menu"
+//                     className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 lg:hidden "
+//                   >
+//                     <svg
+//                       className="h-6 w-6 fill-current"
+//                       xmlns="http://www.w3.org/2000/svg"
+//                       viewBox="0 0 24 24"
+//                     >
+//                       {open && (
+//                         <path
+//                           fillRule="evenodd"
+//                           clipRule="evenodd"
+//                           d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+//                         />
+//                       )}
+//                       {!open && (
+//                         <path
+//                           fillRule="evenodd"
+//                           d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+//                         />
+//                       )}
+//                     </svg>
+//                   </Disclosure.Button>
+//                 </div>
+//                 <div className="flex items-center gap-3">
+//                   <div className="hidden w-full flex-col items-center lg:flex lg:w-auto lg:flex-row ">
+//                     {menu.map((item, index) => (
+//                       <Fragment key={index + item.label}>
+//                         {item.children && item.children.length > 0 ? (
+//                           <DropdownMenu
+//                             menu={item}
+//                             key={index + item.label}
+//                             items={item.children}
+//                             mobile={props.mobile}
+//                           />
+//                         ) : (
+//                           <Link
+//                             href={item.href}
+//                             key={index + item.label}
+//                             className="rounded-full px-5 py-2 font-medium text-gray-600 outline-none ring-blue-100 hover:text-blue-500 focus-visible:text-blue-500 focus-visible:ring-2 dark:text-gray-400"
+//                             target={item.external ? "_blank" : ""}
+//                             rel={item.external ? "noopener" : ""}
+//                           >
+//                             {item.label}
+//                           </Link>
+//                         )}
+//                       </Fragment>
+//                     ))}
+//                   </div>
+//                   <div className="hidden lg:block">
+//                     <form action="/search" method="GET">
+//                       <Search placeholder="Search" />
+//                     </form>
+//                   </div>
+//                 </div>
+//               </div>
+//               <Disclosure.Panel>
+//                 <div className="order-2 -ml-5 mt-5 flex w-full flex-col items-start justify-start lg:hidden">
+//                   {menu.map((item, index) => (
+//                     <Fragment key={index + item.label}>
+//                       {item.children && item.children.length > 0 ? (
+//                         <DropdownMenu
+//                           menu={item}
+//                           key={index + item.label}
+//                           items={item.children}
+//                           mobile={true}
+//                         />
+//                       ) : (
+//                         <Link
+//                           href={item.href}
+//                           key={index + item.label}
+//                           className="rounded-full px-5 py-2 text-sm font-medium text-gray-600 outline-none ring-blue-100 hover:text-blue-500 focus-visible:text-blue-500 focus-visible:ring-2 dark:text-gray-400"
+//                           target={item.external ? "_blank" : ""}
+//                           rel={item.external ? "noopener" : ""}
+//                         >
+//                           {item.label}
+//                         </Link>
+//                       )}
+//                     </Fragment>
+//                   ))}
+//                   <div className="mt-2 px-5">
+//                     <form action="/search" method="GET">
+//                       <Search placeholder="Search" />
+//                     </form>
+//                   </div>
+//                 </div>
+//               </Disclosure.Panel>
+//             </>
+//           )}
+//         </Disclosure>
+//       </nav>
+//     </Container>
+//   );
+// }
