7b2e599bde1d373c9d8d5368687b7984e837004e
Vinleon Ang <javadripdev@gmail.com>
2023-07-29 23:24:13 +0800

    Add responsive py + remove excess py

--- app/components/navigation/NavbarMenu.tsx
+++ app/components/navigation/NavbarMenu.tsx
@@ -12,10 +12,11 @@ import {
 const NavbarMenu = () => {
   const [heading, setHeading] = useState("");
   const [subHeading, setSubHeading] = useState("");
+
   return (
     <>
       <li>
-        <Link href="/" className="inline-block">
+        <Link href="/" className="inline-block py-4 md:py-0">
           Home
         </Link>
       </li>
@@ -23,7 +24,7 @@ const NavbarMenu = () => {
         <div key={index}>
           <div className="text-left md:cursor-pointer group">
             <h1
-              className="flex justify-between items-center md:pr-0 group"
+              className="py-4 md:py-0 flex justify-between items-center group"
               onClick={() => {
                 heading !== link.name ? setHeading(link.name) : setHeading("");
                 setSubHeading("");
@@ -42,20 +43,21 @@ const NavbarMenu = () => {
                   }`}
                 ></ion-icon> */}
               </span>
-              <span className="text-xl md:mt-1 md:ml-2  md:block hidden group-hover:rotate-180 group-hover:-mt-2">
+              <span className="md:ml-1  md:block hidden">
+                <ChevronDownIcon className="h-4" />
                 {/* <ion-icon name="chevron-down"></ion-icon> */}
               </span>
             </h1>
             {link.submenu && (
               <div>
-                <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
+                <div className="absolute top-11 hidden group-hover:md:block hover:md:block">
                   <div className="py-3">
                     <div
                       className="w-4 h-4 left-3 absolute
-                    mt-1 bg-white rotate-45"
+                    mt-1 bg-orange-200 rotate-45"
                     ></div>
                   </div>
-                  <div className="bg-white p-5 grid grid-cols-3 gap-8">
+                  <div className="bg-purple-100 p-5 grid grid-cols-3 gap-8">
                     {link.sublinks.map((mysublinks, index) => (
                       <div key={index}>
                         <h1 className="text-lg font-semibold">
@@ -97,19 +99,24 @@ const NavbarMenu = () => {
                         ? setSubHeading(slinks.Head)
                         : setSubHeading("")
                     }
-                    className="pl-7 font-semibold md:pr-0 flex justify-between items-center"
+                    className="px-8 py-2 flex justify-between items-center"
                   >
                     {slinks.Head}
 
-                    {/* <span className="text-xl md:mt-1 md:ml-2 inline">
-                      <ion-icon
+                    <span className="text-xl inline">
+                      {subHeading === link.name ? (
+                        <ChevronUpIcon className="h-8" />
+                      ) : (
+                        <ChevronDownIcon className="h-8" />
+                      )}
+                      {/* <ion-icon
                         name={`${
                           subHeading === slinks.Head
                             ? "chevron-up"
                             : "chevron-down"
                         }`}
-                      ></ion-icon>
-                    </span> */}
+                      ></ion-icon> */}
+                    </span>
                   </h1>
                   <div
                     className={`${
