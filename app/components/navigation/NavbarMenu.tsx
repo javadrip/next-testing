d8dcb155b99f6c8739486c775392dea7d5fe45de
Vinleon Ang <javadripdev@gmail.com>
2023-07-30 16:01:31 +0800

    Add dynamic behaviour to submenu conditional rendering

--- app/components/navigation/NavbarMenu.tsx
+++ app/components/navigation/NavbarMenu.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Link from "next/link";
 import { Links } from "./Links";
-import {
-  Bars3Icon,
-  ChevronUpIcon,
-  ChevronDownIcon,
-} from "@heroicons/react/24/outline";
+import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 
 const NavbarMenu = () => {
   const [heading, setHeading] = useState("");
@@ -23,46 +19,42 @@ const NavbarMenu = () => {
       {Links.map((link, index) => (
         <div key={index}>
           <div className="text-left md:cursor-pointer group">
-            <h1
-              className="py-4 md:py-0 flex justify-between items-center group"
-              onClick={() => {
-                heading !== link.name ? setHeading(link.name) : setHeading("");
-                setSubHeading("");
-              }}
-            >
-              {link.name}
-              <span className="text-xl md:hidden inline">
-                {heading === link.name ? (
-                  <ChevronUpIcon className="h-8" />
-                ) : (
-                  <ChevronDownIcon className="h-8" />
-                )}
-                {/* <ion-icon
-                  name={`${
-                    heading === link.name ? "chevron-up" : "chevron-down"
-                  }`}
-                ></ion-icon> */}
-              </span>
-              <span className="md:ml-1  md:block hidden">
-                <ChevronDownIcon className="h-4" />
-                {/* <ion-icon name="chevron-down"></ion-icon> */}
-              </span>
-            </h1>
-            {link.submenu && (
-              <div>
-                <div className="absolute top-11 hidden group-hover:md:block hover:md:block">
-                  <div className="py-3">
-                    <div
-                      className="w-4 h-4 left-3 absolute
-                    mt-1 bg-orange-200 rotate-45"
-                    ></div>
-                  </div>
-                  <div className="bg-purple-100 p-5 grid grid-cols-3 gap-8">
+            {link.sublinks && link.sublinks.length > 0 ? (
+              <Fragment>
+                <h1
+                  className="bg-yellow-100 py-4 md:py-0 flex justify-between items-center group"
+                  onClick={() => {
+                    heading !== link.name
+                      ? setHeading(link.name)
+                      : setHeading("");
+                    setSubHeading("");
+                  }}
+                >
+                  {link.name}
+                  <span className="text-xl md:hidden inline">
+                    {heading === link.name ? (
+                      <ChevronUpIcon className="h-8" />
+                    ) : (
+                      <ChevronDownIcon className="h-8" />
+                    )}
+                  </span>
+                  <span className="md:ml-1 md:block hidden">
+                    <ChevronDownIcon className="h-4" />
+                    {/* <ion-icon name="chevron-down"></ion-icon> */}
+                  </span>
+                </h1>
+
+                {/* ================================= DESKTOP SUBMENU ================================= */}
+                <div className="absolute top-10 hidden group-hover:md:block hover:md:block">
+                  <div className="py-3"></div>
+                  <div className="bg-purple-300 p-4 gap-8">
+                    {/* ============================= DESKTOP SUBMENU ITEMS ============================= */}
                     {link.sublinks.map((mysublinks, index) => (
                       <div key={index}>
                         <h1 className="text-lg font-semibold">
                           {mysublinks.Head}
                         </h1>
+                        {/* ============================= DESKTOP SUBLINKS ============================= */}
                         {mysublinks.sublink.map((slink, index) => (
                           <li
                             className="text-sm text-gray-600 my-2.5"
@@ -80,58 +72,60 @@ const NavbarMenu = () => {
                     ))}
                   </div>
                 </div>
-              </div>
-            )}
-          </div>
-          {/* Mobile menus */}
-          <div
-            className={`
-            ${heading === link.name ? "md:hidden" : "hidden"}
-          `}
-          >
-            {/* sublinks */}
-            {link.sublinks.map((slinks, index) => (
-              <div key={index}>
-                <div>
-                  <h1
-                    onClick={() =>
-                      subHeading !== slinks.Head
-                        ? setSubHeading(slinks.Head)
-                        : setSubHeading("")
-                    }
-                    className="px-8 py-2 flex justify-between items-center"
-                  >
-                    {slinks.Head}
-
-                    <span className="text-xl inline">
-                      {subHeading === link.name ? (
-                        <ChevronUpIcon className="h-8" />
-                      ) : (
-                        <ChevronDownIcon className="h-8" />
-                      )}
-                      {/* <ion-icon
-                        name={`${
-                          subHeading === slinks.Head
-                            ? "chevron-up"
-                            : "chevron-down"
-                        }`}
-                      ></ion-icon> */}
-                    </span>
-                  </h1>
-                  <div
-                    className={`${
-                      subHeading === slinks.Head ? "md:hidden" : "hidden"
-                    }`}
-                  >
-                    {slinks.sublink.map((slink, index) => (
-                      <li className="py-3 pl-14" key={index}>
-                        <Link href={slink.link}>{slink.name}</Link>
-                      </li>
-                    ))}
-                  </div>
+                <div
+                  className={` ${
+                    heading === link.name ? "md:hidden" : "hidden"
+                  }`}
+                >
+                  {/* ================================= MOBILE SUBMENU ================================= */}
+                  {link.sublinks.map((slinks, index) => (
+                    <div key={index}>
+                      <div>
+                        <h1
+                          onClick={() =>
+                            subHeading !== slinks.Head
+                              ? setSubHeading(slinks.Head)
+                              : setSubHeading("")
+                          }
+                          className="px-8 py-2 flex justify-between items-center"
+                        >
+                          {slinks.Head}
+                          <span className="text-xl inline">
+                            {subHeading === link.name ? (
+                              <ChevronUpIcon className="h-8" />
+                            ) : (
+                              <ChevronDownIcon className="h-8" />
+                            )}
+                          </span>
+                        </h1>
+                        <div
+                          className={`${
+                            subHeading === slinks.Head ? "md:hidden" : "hidden"
+                          }`}
+                        >
+                          {/* ============================= MOBILE SUBLINKS ============================= */}
+                          {slinks.sublink.map((slink, index) => (
+                            <li className="py-3 pl-14" key={index}>
+                              <Link href={slink.link}>{slink.name}</Link>
+                            </li>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              </div>
-            ))}
+              </Fragment>
+            ) : (
+              <Link
+                href={link.href}
+                key={index + link.name}
+                className="bg-yellow-100 py-4 md:py-0 flex"
+                target={link.external ? "_blank" : ""}
+                rel={link.external ? "noopener" : ""}
+              >
+                {link.name}
+              </Link>
+            )}
           </div>
         </div>
       ))}
@@ -140,6 +134,149 @@ const NavbarMenu = () => {
 };
 
 export default NavbarMenu;
+
+// =========================== V2 NAVBAR ==============================
+
+// "use client";
+
+// import React, { useState } from "react";
+// import Link from "next/link";
+// import { Links } from "./Links";
+// import {
+//   Bars3Icon,
+//   ChevronUpIcon,
+//   ChevronDownIcon,
+// } from "@heroicons/react/24/outline";
+
+// const NavbarMenu = () => {
+//   const [heading, setHeading] = useState("");
+//   const [subHeading, setSubHeading] = useState("");
+
+//   return (
+//     <>
+//       <li>
+//         <Link href="/" className="inline-block py-4 md:py-0">
+//           Home
+//         </Link>
+//       </li>
+//       {Links.map((link, index) => (
+//         <div key={index}>
+//           <div className="text-left md:cursor-pointer group">
+//             <h1
+//               className="py-4 md:py-0 flex justify-between items-center group"
+//               onClick={() => {
+//                 heading !== link.name ? setHeading(link.name) : setHeading("");
+//                 setSubHeading("");
+//               }}
+//             >
+//               {link.name}
+//               <span className="text-xl md:hidden inline">
+//                 {heading === link.name ? (
+//                   <ChevronUpIcon className="h-8" />
+//                 ) : (
+//                   <ChevronDownIcon className="h-8" />
+//                 )}
+//                 {/* <ion-icon
+//                   name={`${
+//                     heading === link.name ? "chevron-up" : "chevron-down"
+//                   }`}
+//                 ></ion-icon> */}
+//               </span>
+//               <span className="md:ml-1 md:block hidden">
+//                 <ChevronDownIcon className="h-4" />
+//                 {/* <ion-icon name="chevron-down"></ion-icon> */}
+//               </span>
+//             </h1>
+
+//             {/* ============================= SUBMENU ============================= */}
+//             {link.submenu && (
+//               <div>
+//                 <div className="absolute top-10 hidden group-hover:md:block hover:md:block">
+//                   <div className="py-3"></div>
+//                   <div className="bg-purple-100 p-4 gap-8">
+//                     {/* ============================= SUBMENU ============================= */}
+//                     {link.sublinks.map((mysublinks, index) => (
+//                       <div key={index}>
+//                         <h1 className="text-lg font-semibold">
+//                           {mysublinks.Head}
+//                         </h1>
+//                         {mysublinks.sublink.map((slink, index) => (
+//                           <li
+//                             className="text-sm text-gray-600 my-2.5"
+//                             key={index}
+//                           >
+//                             <Link
+//                               href={slink.link}
+//                               className="hover:text-primary"
+//                             >
+//                               {slink.name}
+//                             </Link>
+//                           </li>
+//                         ))}
+//                       </div>
+//                     ))}
+//                   </div>
+//                 </div>
+//               </div>
+//             )}
+//           </div>
+//           {/* Mobile menus */}
+//           <div
+//             className={`
+//             ${heading === link.name ? "md:hidden" : "hidden"}
+//           `}
+//           >
+//             {/* sublinks */}
+//             {link.sublinks.map((slinks, index) => (
+//               <div key={index}>
+//                 <div>
+//                   <h1
+//                     onClick={() =>
+//                       subHeading !== slinks.Head
+//                         ? setSubHeading(slinks.Head)
+//                         : setSubHeading("")
+//                     }
+//                     className="px-8 py-2 flex justify-between items-center"
+//                   >
+//                     {slinks.Head}
+
+//                     <span className="text-xl inline">
+//                       {subHeading === link.name ? (
+//                         <ChevronUpIcon className="h-8" />
+//                       ) : (
+//                         <ChevronDownIcon className="h-8" />
+//                       )}
+//                       {/* <ion-icon
+//                         name={`${
+//                           subHeading === slinks.Head
+//                             ? "chevron-up"
+//                             : "chevron-down"
+//                         }`}
+//                       ></ion-icon> */}
+//                     </span>
+//                   </h1>
+//                   <div
+//                     className={`${
+//                       subHeading === slinks.Head ? "md:hidden" : "hidden"
+//                     }`}
+//                   >
+//                     {slinks.sublink.map((slink, index) => (
+//                       <li className="py-3 pl-14" key={index}>
+//                         <Link href={slink.link}>{slink.name}</Link>
+//                       </li>
+//                     ))}
+//                   </div>
+//                 </div>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default NavbarMenu;
 
 // ================================ ORIGINAL NAVBAR ================================ //
 
