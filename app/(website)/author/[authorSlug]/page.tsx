422de890a1fcedc7971faa1f0a8a8a52b8b8b5e1
Vinleon Ang <javadripdev@gmail.com>
2023-08-02 22:39:03 +0800

    Remove old code

--- app/(website)/author/[authorSlug]/page.tsx
+++ app/(website)/author/[authorSlug]/page.tsx
@@ -2,13 +2,11 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
-import { getAuthor } from "@/sanity/sanity-utils";
-import { getPaginatedAuthorPostsBySlug } from "@/sanity/client";
-
-import PostListing from "@/app/components/post/PostListing";
 import Container from "@/app/components/container";
 import AuthorProfile from "./AuthorProfile";
 import AuthorPosts from "./AuthorPosts";
+
+import { getPaginatedAuthorPostsBySlug } from "@/sanity/client";
 
 import type { Author } from "@/types/Author";
 import type { Post } from "@/types/Post";
@@ -40,23 +38,6 @@ export default async function Author({ params: { authorSlug } }: Props) {
     <Container>
       <AuthorProfile authorSlug={authorSlug} />
       <AuthorPosts posts={posts} authorSlug={authorSlug} />
-
-      {/* AUTHOR POSTS ORIGINAL */}
-      {/* <div className="grid gap-10 md:grid-cols-3 lg:gap-10 ">
-        <Suspense fallback={<h2>Loading...</h2>}>
-          {posts.map(post => (
-            <PostListing key={post._id} post={post} aspect="square" />
-          ))}
-        </Suspense>
-      </div>
-      <div className="mt-10 flex justify-center">
-        <Link
-          href="/archive"
-          className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
-        >
-          <span>View all Posts</span>
-        </Link>
-      </div> */}
     </Container>
   );
 }
