a7038e5e8cb363509fd5ccc38604aa3dd802dbeb
Vinleon Ang <javadripdev@gmail.com>
2023-08-04 17:10:06 +0800

    Remove nomargin

--- app/components/ui/CategoryLabel.tsx
+++ app/components/ui/CategoryLabel.tsx
@@ -7,21 +7,19 @@ import { Category } from "@/types/Category";
 
 interface Props {
   categories: Category[];
-  nomargin?: boolean;
   hideCategoryLabel?: boolean;
 }
 
 export default function CategoryLabel({
   categories,
-  nomargin = false,
   hideCategoryLabel,
 }: Props) {
   return (
-    <div className={clsx("flex gap-3", hideCategoryLabel && "hidden")}>
+    <div className={clsx("flex gap-2", hideCategoryLabel && "hidden")}>
       {categories?.length &&
         categories.slice(0).map((category, index) => (
           <Link href={`/${category.slug.current}`} key={index}>
-            <CategoryLabelStyles nomargin={nomargin} color={category.color}>
+            <CategoryLabelStyles color={category.color}>
               {category.title}
             </CategoryLabelStyles>
           </Link>
