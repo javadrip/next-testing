ae8b5fddedb5103ac1e54867526262a05543a615
Vinleon Ang <javadripdev@gmail.com>
2023-08-07 15:50:43 +0800

    Add smooth scrolling upon clicking on TOC links

--- app/components/post/TableOfContents.tsx
+++ app/components/post/TableOfContents.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { getPostHeadings } from "@/sanity/client";
 import { Headings } from "@/types/Headings";
 
@@ -19,18 +21,35 @@ export default async function TableOfContents({
 
   const headings = (await headingsData).headings;
 
+  const scrollToHeading = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
+    // div is necessary for the sticky to work
     <div>
-      <h1>Table of Contents</h1>
-      <ul>
-        {headings.map((heading, index) => (
-          <li key={index}>
-            <a href={`#${speakingurl(heading.children[0].text)}`}>
-              {heading.children[0].text}
-            </a>
-          </li>
-        ))}
-      </ul>
+      {/* div that only appears when the post appears and sticks to the side of the post */}
+      <div className="hidden xl:block sticky top-20 max-w-fit mx-auto mt-4">
+        <h1>Table of Contents</h1>
+        <ul>
+          {headings.map((heading, index) => (
+            <li key={index}>
+              <a
+                href={`#${speakingurl(heading.children[0].text)}`}
+                onClick={e => {
+                  e.preventDefault();
+                  scrollToHeading(speakingurl(heading.children[0].text));
+                }}
+              >
+                {heading.children[0].text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
