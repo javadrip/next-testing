1677aad66101c385a35418a2777f2b492b0eae4a
Vinleon Ang <javadripdev@gmail.com>
2023-11-02 08:16:42 +0800

    Make hotspot work

--- lib/urlFor.ts
+++ lib/urlFor.ts
@@ -16,6 +16,7 @@ const builder = imageUrlBuilder(client);
 // TODO: Give source a type
 export const urlForImage = (source: any): ImageData | undefined => {
   if (!source || !source.asset) return;
+  console.log("source", source);
 
   const dimensions = source?.asset?._ref.split("-")[2];
 
@@ -27,8 +28,9 @@ export const urlForImage = (source: any): ImageData | undefined => {
     .image(source)
     .auto("format")
     .width(Math.min(width, 2000))
-    // Adding hotspot info did not work. Also broke other images.
-    // .focalPoint(source?.hotspot?.x, source?.hotspot?.y)
+    // .crop is required for focalPoint and hotspot to work
+    .crop("focalpoint")
+    // .fit("crop")
     .url();
 
   return {
