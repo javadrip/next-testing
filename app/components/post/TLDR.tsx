541069a778ee211fe5e6163bb98c2fc362d99d91
Vinleon Ang <javadripdev@gmail.com>
2023-11-05 12:14:53 +0800

    Rename tldrBlock to tldrData for better clarity

--- app/components/post/TLDR.tsx
+++ app/components/post/TLDR.tsx
@@ -7,15 +7,15 @@ type Props = {
 };
 
 export default async function TLDR({ postSlug, categorySlug }: Props) {
-  const tldrBlock = await getPostTldr(categorySlug, postSlug);
+  const tldrData = await getPostTldr(categorySlug, postSlug);
 
-  console.log(tldrBlock.tldr);
+  console.log(tldrData.tldr);
 
   return (
     // prose is a class from tailwindcss https://tailwindcss.com/docs/typography-plugin
     // prose is required for PortableText to render properly
     <div className="prose mx-auto my-4 dark:prose-invert prose-a:text-blue-600">
-      {tldrBlock.tldr && <PortableText value={tldrBlock.tldr} />}
+      {tldrData.tldr && <PortableText value={tldrData.tldr} />}
     </div>
   );
 }
