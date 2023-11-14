4a6816b906f55036b630b476fa0ac8b97021eb1e
Vinleon Ang <javadripdev@gmail.com>
2023-11-14 19:41:04 +0800

    Remove console.log

--- app/components/post/TLDR.tsx
+++ app/components/post/TLDR.tsx
@@ -9,12 +9,15 @@ type Props = {
 export default async function TLDR({ postSlug, categorySlug }: Props) {
   const tldrData = await getPostTldr(categorySlug, postSlug);
 
-  console.log(tldrData.tldr);
-
   return (
     // prose is a class from tailwindcss https://tailwindcss.com/docs/typography-plugin
     // prose is required for PortableText to render properly
-    <div className="prose mx-auto my-4 dark:prose-invert prose-a:text-blue-600">
+    <div className="prose mx-auto my-4 dark:prose-invert prose-a:text-blue-600 border-4 p-2 rounded-lg">
+      <div>
+        <p className="text-brand-primary text-2xl font-semibold tracking-tight mt-2 ml-2 mb-2">
+          TL;DR
+        </p>
+      </div>
       {tldrData.tldr && <PortableText value={tldrData.tldr} />}
     </div>
   );
