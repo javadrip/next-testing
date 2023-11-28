46b0e6009a4d666018a1c222ef9ba11016c795cb
Vinleon Ang <javadripdev@gmail.com>
2023-11-28 15:05:53 +0800

    Create useReadingProgressBar hook

---
+++ src/hooks/useReadingProgressbar.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+/**
+ * React Hook to get the scroll percentage from the page, returns value from 0 to 100
+ */
+export function useReadingProgress() {
+  const [completion, setCompletion] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    function updateScrollCompletion() {
+      // see how much we have scrolled
+      const currentProgress = window.scrollY;
+      // see how much total scroll is available
+      const scrollHeight = document.body.scrollHeight - window.innerHeight;
+      if (scrollHeight) {
+        setCompletion(
+          Number((currentProgress / scrollHeight).toFixed(2)) * 100
+        );
+      }
+    }
+
+    function handleRouteChange() {
+      setCompletion(0); // Reset completion when the route changes
+    }
+
+    // Add scroll and route change event listeners
+    window.addEventListener("scroll", updateScrollCompletion);
+    // TODO: Clarify how to change this to suit next/navigation
+    // router.events.on("routeChangeStart", handleRouteChange);
+
+    // Remove event listeners on unmount
+    return () => {
+      window.removeEventListener("scroll", updateScrollCompletion);
+      // TODO: Clarify how to change this to suit next/navigation
+      // router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router]);
+
+  return completion;
+}
