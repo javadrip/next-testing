c8d5cb3695b9446f00f44636d409c0c302b9d937
Vinleon Ang <javadripdev@gmail.com>
2023-11-29 09:43:33 +0800

    Add useScrollVisibility to  ReadingProgressBar

--- app/components/post/ReadingProgressBar.tsx
+++ app/components/post/ReadingProgressBar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useReadingProgress } from "@/src/hooks/useReadingProgressbar";
+import useScrollVisibility from "@/src/hooks/useScrollVisibility";
 
 export default function ReadingProgressBar() {
   const completion = useReadingProgress();
+  const show = useScrollVisibility();
 
   return (
     <header
@@ -11,7 +13,9 @@ export default function ReadingProgressBar() {
         backgroundSize: `${completion}%`,
         backgroundRepeat: "no-repeat",
       }}
-      className="fixed top-0 h-14 w-full bg-gradient-to-r from-red-500/30 from-30% to-blue-500/50 to-100% transition-all duration-500 ease-in-out z-50"
+      className={`fixed h-14 w-full bg-gradient-to-r from-red-500/30 from-30% to-blue-500/50 to-100% transition-all duration-500 ease-in-out z-50 ${
+        show ? "top-0" : "-top-12"
+      }`}
     ></header>
   );
 }
