7ada83013049c52c1916e4db8e01b489a84c9913
Vinleon Ang <javadripdev@gmail.com>
2023-11-29 09:42:51 +0800

    Create useScrollVisibility hook

---
+++ src/hooks/useScrollVisibility.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const useScrollVisibility = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    if (typeof window !== "undefined") {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        // Scrolling down
+        setShowNavbar(false);
+      } else {
+        // Scrolling up
+        setShowNavbar(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  });
+
+  return showNavbar;
+};
+
+export default useScrollVisibility;
