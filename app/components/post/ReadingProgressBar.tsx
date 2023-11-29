f133dd11bb3af63d6fd25fee9697a52199957735
Vinleon Ang <javadripdev@gmail.com>
2023-11-29 16:03:49 +0800

    Edit transition and constant name

--- app/components/post/ReadingProgressBar.tsx
+++ app/components/post/ReadingProgressBar.tsx
@@ -4,18 +4,19 @@ import { useReadingProgress } from "@/src/hooks/useReadingProgressbar";
 import useScrollVisibility from "@/src/hooks/useScrollVisibility";
 
 export default function ReadingProgressBar() {
-  const completion = useReadingProgress();
+  const scrollPercentage = useReadingProgress();
   const show = useScrollVisibility();
 
   return (
-    <header
+    <div
       style={{
-        backgroundSize: `${completion}%`,
+        backgroundSize: `${scrollPercentage}%`,
         backgroundRepeat: "no-repeat",
       }}
-      className={`fixed h-14 w-full bg-gradient-to-r from-red-500/30 from-30% to-blue-500/50 to-100% transition-all duration-500 ease-in-out z-50 ${
+      // `transition-[top]` determines how the top property changes over time. It is required to make sure the navbar slides in and out smoothly.
+      className={`fixed h-14 w-full bg-gradient-to-r from-red-500/30 from-30% to-blue-500/50 to-100% transition-[top] ease-in-out delay-150 z-50 ${
         show ? "top-0" : "-top-12"
       }`}
-    ></header>
+    ></div>
   );
 }
